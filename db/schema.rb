aa297896913c1150f24aa19bbb61e3624fa3f7c8
Starotitorov Artem <starotitorov1997@gmail.com>
2015-12-22 15:11:52 +0300

    Some additional changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151128120312) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "browser_games", force: :cascade do |t|
     t.string   "name",            default: "one of my games"
     t.string   "status",          default: "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20151128120312) do
     t.datetime "updated_at",                                                                                                                       null: false
   end
 
-  add_index "browser_games", ["user_id", "created_at"], name: "index_browser_games_on_user_id_and_created_at"
+  add_index "browser_games", ["user_id", "created_at"], name: "index_browser_games_on_user_id_and_created_at", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -38,7 +41,7 @@ ActiveRecord::Schema.define(version: 20151128120312) do
     t.boolean  "admin",               default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
